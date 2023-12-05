b841d6069ef058555f35bbe750c011c04c90a53f
Tandu Yanmick Ateh <95031570+G-GraceCode@users.noreply.github.com>
2023-12-05 14:06:33 +0000

    Update 3 files

---
+++ routes.js
@@ -0,0 +1,93 @@
+const passport = require("passport");
+
+module.exports = function (app, myDataBase) {
+  // 1, 2, Set up the engine template and template Power
+
+  //change the response to render the Pug template
+  app.route("/").get((req, res) => {
+    res.render("index", {
+      title: "Connected to Database",
+      message: "Please login",
+      showLogin: true,
+      showRegistration: true,
+    });
+  });
+
+  // How to Use Passport Strategies, to login a user
+  app
+    .route("/login")
+    .post(
+      passport.authenticate("local", { failureRedirect: "/" }),
+      (req, res) => {
+        res.redirect("/profile");
+      },
+    );
+
+  // user profile route /profile
+  app.route("/profile").get(ensureAuthenticated, (req, res) => {
+    res.render("profile", { username: req.user.username });
+  });
+
+  // login a user route /logout
+  app.route("/logout").get((req, res) => {
+    req.logout();
+    res.redirect("/");
+  });
+
+  // middleware for missing pages
+  app.use((req, res, next) => {
+    res.status(404).type("text").send("Not Found");
+  });
+
+  /* Resgistration of new new users logic
+  1. resgister the user
+  2. Authenticate the new user
+  3. Redirect to /profile
+
+  Then we are going to hash the pasword before saving to our database
+  */
+  // Route /resgister
+  app.route("/register").post(
+    (req, res, next) => {
+      myDataBase.findOne({ username: req.body.username }, (user, err) => {
+        if (err) {
+          next(err);
+        } else if (user) {
+          res.redirect("/");
+        } else {
+          const hash = bcrypt.hashSync(req.body.password, 12);
+          myDataBase.insertOne(
+            {
+              username: req.body.username,
+              password: hash,
+            },
+            (err, doc) => {
+              if (err) {
+                res.redirect("/");
+              } else {
+                // The inserted document is held within
+                // the ops property of the doc
+                next(null, doc.ops[0]);
+              }
+            },
+          );
+        }
+      });
+    },
+    passport.authenticate(
+      "local",
+      { failureRedirect: "/" },
+      (req, res, next) => {
+        res.redirect("/profile");
+      },
+    ),
+  );
+
+  // 8) Create New Middleware
+  function ensureAuthenticated(req, res, next) {
+    if (req.isAuthenticated()) {
+      return next();
+    }
+    res.redirect("/");
+  }
+};
--- server.js
+++ server.js
@@ -1,12 +1,12 @@
 "use strict";
 require("dotenv").config();
+const routes = require("./routes.js");
+const auth = require("./auth.js");
 const express = require("express");
 const myDB = require("./connection");
 const { ObjectID } = require("mongodb");
 const fccTesting = require("./freeCodeCamp/fcctesting.js");
 const bcrypt = require("bcrypt");
-
-const LocalStrategy = require("passport-local");
 
 // setting up passport
 const session = require("express-session");
@@ -43,126 +43,15 @@ app.set("views", "./views/pug");
 /* 5) Implement the serialization of a Pasport User*/
 myDB(async (client) => {
   const myDataBase = await client.db("database").collection("users");
-  // 1, 2, Set up the engine template and template Power
 
-  //change the response to render the Pug template
-  app.route("/").get((req, res) => {
-    res.render("index", {
-      title: "Connected to Database",
-      message: "Please login",
-      showLogin: true,
-      showRegistration: true,
-    });
-  });
-
-  // Serialization and deserialization are now here...
-  passport.serializeUser((user, done) => {
-    done(null, user._id);
-  });
-
-  passport.deserializeUser((id, done) => {
-    myDataBase.findOne({ _id: new ObjectID(id) }, (err, doc) => {
-      done(null, doc);
-    });
-  });
-
-  // How to Use Passport Strategies, to login a user
-  app
-    .route("/login")
-    .post(
-      passport.authenticate("local", { failureRedirect: "/" }),
-      (req, res) => {
-        res.redirect("/profile");
-      },
-    );
-
-  // user profile route /profile
-  app.route("/profile").get(ensureAuthenticated, (req, res) => {
-    res.render("profile", { username: req.user.username });
-  });
-
-  // login a user route /logout
-  app.route("/logout").get((req, res) => {
-    req.logout();
-    res.redirect("/");
-  });
-
-  // middleware for missing pages
-  app.use((req, res, next) => {
-    res.status(404).type("text").send("Not Found");
-  });
-
-  /* Resgistration of new new users logic
-  1. resgister the user
-  2. Authenticate the new user
-  3. Redirect to /profile
-
-  Then we are going to hash the pasword before saving to our database
-  */
-  // Route /resgister
-  app.route("/register").post(
-    (req, res, next) => {
-      myDataBase.findOne({ username: req.body.username }, (user, err) => {
-        if (err) {
-          next(err);
-        } else if (user) {
-          res.redirect("/");
-        } else {
-          const hash = bcrypt.hashSync(req.body.password, 12);
-          myDataBase.insertOne(
-            {
-              username: req.body.username,
-              password: hash,
-            },
-            (err, doc) => {
-              if (err) {
-                res.redirect("/");
-              } else {
-                // The inserted document is held within
-                // the ops property of the doc
-                next(null, doc.ops[0]);
-              }
-            },
-          );
-        }
-      });
-    },
-    passport.authenticate(
-      "local",
-      { failureRedirect: "/" },
-      (req, res, next) => {
-        res.redirect("/profile");
-      },
-    ),
-  );
-
-  /*6) authentication Strategies, coparing the hash password*/
-  passport.use(
-    new LocalStrategy((username, password, done) => {
-      myDataBase.findOne({ username: username }, (err, user) => {
-        console.log(`User ${username} attempted to log in.`);
-        if (err) return done(err);
-        if (!user) return done(null, false);
-        if (!bcrypt.compareSync(password, user.password))
-          return done(null, false);
-        return done(null, user);
-      });
-    }),
-  );
+  routes(app, myDataBase);
+  auth(app, myDataBase);
 }).catch((e) => {
-  // 1, 2, Set up the engine template and template Power
+  // Set up the engine Template and Template power
   app.route("/").get((req, res) => {
     res.render("index", { title: e, message: "Unable to connect to database" });
   });
 });
-
-// 8) Create New Middleware
-function ensureAuthenticated(req, res, next) {
-  if (req.isAuthenticated()) {
-    return next();
-  }
-  res.redirect("/");
-}
 
 const PORT = process.env.PORT || 3000;
 app.listen(PORT, () => {
