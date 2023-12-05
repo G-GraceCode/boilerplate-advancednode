9cb3f14cc0ba53a900cea27e2fea5591199d8d6b
Tandu Yanmick Ateh <95031570+G-GraceCode@users.noreply.github.com>
2023-12-05 12:26:48 +0000

    Update 4 files

--- server.js
+++ server.js
@@ -5,6 +5,8 @@ const myDB = require("./connection");
 const { ObjectID } = require("mongodb");
 const fccTesting = require("./freeCodeCamp/fcctesting.js");
 
+const LocalStrategy = require("passport-local");
+
 // setting up passport
 const session = require("express-session");
 const passport = require("passport");
@@ -31,31 +33,23 @@ passport.session();
 
 // passport setting end,
 
-/*4)  Serialization of a User Object: Serialization and deserialization are important concepts in regards to authentication.  serial convert content into small key that can then be deserialized into the original object*/
-
-// Serialization of a User Object
-// passport.serializeUser((user, done) => {
-//   done(null, user._id);
-// });
-
-// passport.deserializeUser((id, done) => {
-//   myDataBase.findOne({ _id: new objectID(id) }, (err, doc) => {
-//     done(null, doc);
-//   });
-// });
+/* 4)  Serialization of a User Object: Serialization and deserialization are important concepts in regards to authentication.  serial convert content into small key that can then be deserialized into the original object*/
+// see in the Implement part
 
 app.set("view engine", "pug");
 app.set("views", "./views/pug");
 
-/*Implement the serialization of a Pasport User*/
+/*5) Implement the serialization of a Pasport User*/
 myDB(async (client) => {
   const myDataBase = await client.db("database").collection("users");
   // 1, 2, Set up the engine template and template Power
+
   //change the response to render the Pug template
   app.route("/").get((req, res) => {
     res.render("index", {
       title: "Connected to Database",
       message: "Please login",
+      showLogin: true,
     });
   });
 
@@ -65,10 +59,37 @@ myDB(async (client) => {
   });
 
   passport.deserializeUser((id, done) => {
-    myDataBase.findOne({ _id: new objectID(id) }, (err, doc) => {
+    myDataBase.findOne({ _id: new ObjectID(id) }, (err, doc) => {
       done(null, doc);
     });
   });
+
+  // How to Use Passport Strategies
+  app
+    .route("/login")
+    .post(
+      passport.authenticate("local", { failureRedirect: "/" }),
+      (req, res) => {
+        res.redirect("/profile");
+      },
+    );
+
+  app.route("/profile").get((req, res) => {
+    res.render("profile.pu");
+  });
+
+  /*6) authentication Strategies*/
+  passport.use(
+    new LocalStrategy((username, password, done) => {
+      myDataBase.findOne({ username: username }, (err, user) => {
+        console.log(`User ${username} attempted to log in.`);
+        if (err) return done(err);
+        if (!user) return done(null, false);
+        if (password !== user.password) return done(null, false);
+        return done(null, user);
+      });
+    }),
+  );
 }).catch((e) => {
   // 1, 2, Set up the engine template and template Power
   app.route("/").get((req, res) => {
