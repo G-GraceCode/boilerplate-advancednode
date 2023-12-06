61f5d22b3cb75a35f8197467e9cb24d973ce6fba
Tandu Yanmick Ateh <95031570+G-GraceCode@users.noreply.github.com>
2023-12-06 23:14:38 +0000

    Update auth.js and index.pug

--- auth.js
+++ auth.js
@@ -13,6 +13,7 @@ module.exports = function (app, myDataBase) {
 
   passport.deserializeUser((id, done) => {
     myDataBase.findOne({ _id: new ObjectID(id) }, (err, doc) => {
+      if (err) return console.error(err);
       done(null, doc);
     });
   });
@@ -23,9 +24,11 @@ module.exports = function (app, myDataBase) {
       myDataBase.findOne({ username: username }, (err, user) => {
         console.log(`User ${username} attempted to log in.`);
         if (err) return done(err);
+
         if (!user) return done(null, false);
-        if (!bcrypt.compareSync(password, user.password))
+        if (!bcrypt.compareSync(password, user.password)) {
           return done(null, false);
+        }
         return done(null, user);
       });
     }),
